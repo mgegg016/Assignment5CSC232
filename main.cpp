7ee324934148e89327d016d8bf7a66998e49f246
mgegg016 <72768427+mgegg016@users.noreply.github.com>
2020-10-13 17:29:06 -0500

    V1.5 Mike

--- main.cpp
+++ main.cpp
@@ -22,7 +22,10 @@ int main(){
     string tempAN;
     Bank tempAccount;
     //readFile function needs to be called first
+    //v1.6
+
     while(play){
+        cout<<"Version 1.5"<<endl;
         cout<<"Welcome to the bank account system, would you like to [1] Open an account, [2] Login, [3] exit: ";
         cin >> choice;
         if (choice==1||choice==2||choice==3){
@@ -37,7 +40,7 @@ int main(){
                         //todo login stuffs;
                     }
                     else{
-                        cout<<"Invalid input!";
+                        cout<<"Invalid input!"<<endl;
                         play=true;
                         break;
                     }
