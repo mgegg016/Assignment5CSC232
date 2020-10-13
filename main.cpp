821575943cc98096bfc2659c5e8a6c89428e5667
mgegg016 <72768427+mgegg016@users.noreply.github.com>
2020-10-13 17:50:06 -0500

    V1.7 Mike

--- main.cpp
+++ main.cpp
@@ -25,9 +25,11 @@ int main(){
     //v1.6
     //jay test
     while(play){
-        cout<<"Version 1.6"<<endl;
+        cout<<"Version 1.7"<<endl;
         cout<<"Welcome to the bank account system, would you like to [1] Open an account, [2] Login, [3] exit: ";
         cin >> choice;
+        Checking c1;
+        cout<<c1.getAccntNumber()<<endl;
         if (choice==1||choice==2||choice==3){
             switch(choice){
                 case 1:
